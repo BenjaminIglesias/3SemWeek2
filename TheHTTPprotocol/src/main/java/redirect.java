9918a15350066aed8c3a0e7104a366af0c36c7fd
BenjaminIglesias <cph-bc106@cphbusiness.dk>
2020-09-04 15:51:25 +0200

    lets go

---
+++ TheHTTPprotocol/src/main/java/redirect.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ *
+ * @author Benjamin
+ */
+@WebServlet(urlPatterns = {"/redirect"})
+public class redirect extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        response.sendRedirect("r.html");
+    }
+
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }// </editor-fold>
+
+}
